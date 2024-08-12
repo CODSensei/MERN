8f15591b76b5d798207dff4eea429ef73a252369
CODSensei <krishna2342005@gmail.com>
2024-08-12 16:30:08 +0530

    express api routes

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,21 @@
 require('dotenv').config()
 
 const express = require("express");
+const workoutRoutes = require('./routes/workouts')
 
 //express app being stored in app variable
 const app = express();
 
 //global middleware
+app.use(express.json())
+
 app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()    
 })
 
 //reacting to requests on port 4000 / routes / middleware
-app.get("/", (req, res) => {
-    res.json({ mssg: "Welcome to the App" });  
-});
-
+app.use('/api/workouts',workoutRoutes)
 
 //listening to a certain port number /request
 app.listen(process.env.PORT, () => {
