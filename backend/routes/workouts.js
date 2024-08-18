7852b6fa5dd4be3325bc7c39686f2d6674732e4b
CODSensei <krishna2342005@gmail.com>
2024-08-18 14:54:12 +0530

    backend creation completed

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,30 +1,27 @@
 const express = require("express");
-
 const router = express.Router();
 
+const {
+  getAllWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController.js");
+
 //GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all workouts" });
-});
+router.get("/", getAllWorkouts);
 
 //GET single workout
-router.get("/:id", (req, res) => {
-    res.json({mssg: "GET single workout"})
-});
+router.get("/:id", getWorkout);
 
 //POST new workout
-router.post('/',(req,res) => {
-    res.json({mssg: "POST a new workout"})
-})
+router.post("/", createWorkout);
 
 //DELETE a workout
-router.delete('/:id',(req,res) => {
-    res.json({mssg: "DELETE a workout"})
-})
+router.delete("/:id", deleteWorkout);
 
 //UPDATE a workout
-router.patch('/:id',(req,res) => {
-    res.json({mssg: "PATCH a workout"})
-})
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,32 @@
-
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
-const workoutRoutes = require('./routes/workouts')
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
 //express app being stored in app variable
 const app = express();
 
 //global middleware
-app.use(express.json())
+app.use(express.json());
 
-app.use((req,res,next) => {
-    console.log(req.path, req.method)
-    next()    
-})
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //reacting to requests on port 4000 / routes / middleware
-app.use('/api/workouts',workoutRoutes)
+app.use("/api/workouts", workoutRoutes);
 
-//listening to a certain port number /request
-app.listen(process.env.PORT, () => {
-  console.log("listening on port ",process.env.PORT);
-});
+//DB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listening to a certain port number /request
+    app.listen(process.env.PORT, () => {
+      console.log("connected to DB and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
